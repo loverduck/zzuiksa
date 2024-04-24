4860a9b0119c15dafcdbb44915aeff9bae23ff0d
박이언 <0onionion0@naver.com>
2024-04-24 16:19:09 +0900

    ✨ feat: 생성, 수정, 삭제 시점 저장하는 BaseEntity 추가
    #S10P31A202-135

---
+++ server/src/main/java/com/zzuiksa/server/global/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.zzuiksa.server.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected LocalDateTime updatedAt;
+
+    protected LocalDateTime deletedAt;
+}
