9df1b98efd74d73830332f85ad8755407f512a21
조아영 <aycho3030@gmail.com>
2024-04-26 10:39:16 +0900

    💄 Style: add color constants
    Style: add color constants

---
+++ client/lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Colors
+  static const main600 = Color(0xFF795458);
+  static const main500 = Color(0xFFC08B5C);
+  static const main400 = Color(0xFFED9455);
+  static const main300 = Color(0xFFFFBB70);
+  static const main200 = Color(0xFFFFEC9E);
+  static const main100 = Color(0xFFFFFBDA);
+  static const blue600 = Color(0xFF453F78);
+  static const blue300 = Color(0xFF9BB8CD);
+  static const green300 = Color(0xFFB1C381);
+  static const pink400 = Color(0xFFFF6666);
+  static const pink300 = Color(0xFFFF8989);
+  static const pink200 = Color(0xFFFCAEAE);
+  static const violet300 = Color(0xFFD89ADF);
+  static const textColor = Color(0xFF451E1E);
+}
