9ee580aacd5457b53d9ccca91b7be540d830311e
박이언 <0onionion0@naver.com>
2024-05-03 16:33:35 +0900

    🐛 fix: add '/api' prefix to controller mappings
    #S10P31A202-254-swagger

--- server/src/main/java/com/zzuiksa/server/domain/schedule/controller/ScheduleController.java
+++ server/src/main/java/com/zzuiksa/server/domain/schedule/controller/ScheduleController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/schedules")
+@RequestMapping("/api/schedules")
 @RequiredArgsConstructor
 public class ScheduleController {
 
