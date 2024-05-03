469f08cb3ba768162cb0dc299298dd4862fdb80e
유소연 <jjellyduck@gmail.com>
2024-05-03 17:09:05 +0900

    ✨ feat: add get member api
    #S10P31A202-281

---
+++ server/src/main/java/com/zzuiksa/server/domain/member/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.zzuiksa.server.domain.member.controller;
+
+import com.zzuiksa.server.domain.auth.data.MemberDetail;
+import com.zzuiksa.server.domain.member.data.response.GetMemberResponse;
+import com.zzuiksa.server.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping
+    public GetMemberResponse getMember(@AuthenticationPrincipal MemberDetail memberDetail) {
+        return memberService.get(memberDetail.getMember().getId());
+    }
+}
