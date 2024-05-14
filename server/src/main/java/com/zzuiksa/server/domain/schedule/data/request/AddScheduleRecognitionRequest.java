6f2690cbb82156c50a66c3b52c81ab3674d7ea91
박이언 <0onionion0@naver.com>
2024-05-14 11:06:05 +0900

    🐛 fix: 자연어 일정 등록 API 필수 속성 검사
    #S10P31A202-371-ai

--- server/src/main/java/com/zzuiksa/server/domain/schedule/data/request/AddScheduleRecognitionRequest.java
+++ server/src/main/java/com/zzuiksa/server/domain/schedule/data/request/AddScheduleRecognitionRequest.java
@@ -2,14 +2,21 @@ package com.zzuiksa.server.domain.schedule.data.request;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class AddScheduleRecognitionRequest {
 
+    @NotNull
     private Long categoryId;
 
+    @NotBlank
+    @Size(max = 100)
     private String content;
 
+    @NotNull
     private LocalDate baseDate;
 }
